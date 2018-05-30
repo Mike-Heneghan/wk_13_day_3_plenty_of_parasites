5bdb3df490c09f709b5338ebfc57955033cbc0ee
Mike-Heneghan <m.heneghan@live.co.uk>
2018-05-30 14:50:50 +0100

    set up server & index_router

---
+++ server/routers/index_router.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const MongoClient = require('mongodb').MongoClient;
+const countriesRouter = require('./countries_router');
+
+MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+  const db = client.db('countries');
+  const countriesCollection = db.collection('countries');
+  router.use('/api/countries', countriesRouter(countriesCollection));
+});
+
+module.exports = router;
