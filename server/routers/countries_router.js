f9c11452013cfe54934d9c5866f57385ec8ee0f4
Mike-Heneghan <m.heneghan@live.co.uk>
2018-05-30 16:24:39 +0100

    Added the post route to countries_router upload of lab progress.

--- server/routers/countries_router.js
+++ server/routers/countries_router.js
@@ -8,9 +8,23 @@ const countriesRouter = function(countriesCollection) {
     .find()
     .toArray()
     .then((docs) => res.json(docs))
-
   });
+
+  router.post('/', (req, res) => {
+    const newCountry = req.body;
+    countriesCollection
+    .insertOne(newCountry)
+    .then(
+      countriesCollection
+      .find()
+      .toArray()
+      .then((docs) => res.json(docs))
+    );
+  });
+
   return router;
 };
+
+
 
 module.exports = countriesRouter;
